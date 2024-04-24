ce93b0de0cc572b763d149580e1ce46d7c15b780
PemaLama007 <pema3528@gmail.com>
2024-04-24 15:22:04 +0545

    final admin dashboard completed using MUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import { Route, Routes } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Team from "./scenes/team/Team";
+import Dashboard from "./scenes/dashboard"
+import Sidebar from "./scenes/global/Sidebar";
+import Invoices from "./scenes/invoices/Invoices"
+import Contacts from "./scenes/contacts/Contacts"
+import Form from "./scenes/form/Form"
+import Calendar from "./scenes/calendar/Calendar"
+import Bar from "./scenes/bar/Bar"
+import Line from "./scenes/line/Line"
+import Pie from "./scenes/pie/Pie"
+import Faq from "./scenes/faq/Faq"
+import Geography from "./scenes/geography/Geography"
 
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <div className="app">
+          <Sidebar />
+            <main className="content">
+              <Topbar/>
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/team" element={<Team />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/form" element={<Form />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/faq" element={<Faq />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/line" element={<Line />} />
+                <Route path="/geography" element={<Geography />} />
+              </Routes>
+            </main>
+        </div>
+     </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
